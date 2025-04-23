942a2b256cc37739330302acc8ad9cd39d4f821c
Nishi Sharma <72256682+nishisharma842004@users.noreply.github.com>
2025-04-23 23:05:04 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -113,7 +113,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/nishisharma842004"
                 target="_blank"
                 className="fork-btn-inner"
               >
